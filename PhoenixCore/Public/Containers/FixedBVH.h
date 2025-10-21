b00f98232d3d62253d7a31fa743a1129aa3cc088
James Farris <jlfarris91@gmail.com>
2025-10-21 11:57:36 -0700

    Adding FixedBVH container

---
+++ PhoenixCore/Public/Containers/FixedBVH.h
@@ -0,0 +1,131 @@
+
+#pragma once
+
+#include "CTZ.h"
+#include "PhoenixCore.h"
+#include "PlatformTypes.h"
+#include "Containers/FixedArray.h"
+#include "FixedPoint/FixedVector.h"
+#include "FixedPoint/FixedBox.h"
+
+namespace Phoenix
+{
+    template <class T, uint32 N, class TVec = Vec2, class TIdx = uint16>
+    struct TFixedBVH
+    {
+        using TBox = TFixedBox<TVec>;
+
+        enum class ENodeType : uint8
+        {
+            None,
+            Leaf,
+            Parent
+        };
+
+        struct Node
+        {
+            struct Leaf
+            {
+                T Value;
+            };
+
+            struct Parent
+            {
+                TIdx Left;
+                TIdx Right;
+            };
+
+            union
+            {
+                Leaf Leaf;
+                Parent Parent;
+            };
+
+            TBox Bounds;
+            ENodeType Type = ENodeType::None;
+        };
+
+        TIdx Insert(const TBox& bounds, const T& value)
+        {
+            // uint64 mortonCode = bounds.GetCenter();
+            // CommonPrefix()
+            return Index<TIdx>::None;
+        }
+
+        template <class TList, class TBoundsFunc, class TValueFunc>
+        TIdx InsertRange(
+            const TList& list,
+            TIdx begin,
+            TIdx end,
+            const TBoundsFunc& boundsFunc,
+            const TValueFunc& valueFunc)
+        {
+            return Index<TIdx>::None;
+            // if (begin == end)
+            // {
+            //     return AllocateLeafNode(boundsFunc(list, begin), valueFunc(list, begin));
+            // }
+            //
+            // TIdx m = GetSplitPos(begin, end);
+            // TIdx left = InsertRange(list, begin, m - 1);
+            // TIdx right = InsertRange(list, m, end);
+            // if (left == Index<TIdx>::None || right == Index<TIdx>::None)
+            // {
+            //     return Index<TIdx>::None;
+            // }
+            //
+            // TBox bounds = TBox::Union(Nodes[left].Bounds, Nodes[right].Bounds);
+            // return AllocateParentNode(bounds, left, right);
+        }
+
+        void Reset()
+        {
+            Nodes.Reset();
+        }
+
+        static uint64 CommonPrefix(uint64 a, uint64 b)
+        {
+            // Count leading zeroes of differing bits
+            return CLZ(a ^ b);
+        }
+
+        TIdx GetSplitPos(TIdx begin, TIdx end)
+        {
+            return floor((begin + end) / 2);
+        }
+
+        TIdx AllocateLeafNode(const TBox& bounds, const T& value)
+        {
+            if (Nodes.IsFull())
+            {
+                return Index<TIdx>::None;
+            }
+
+            Node& node = Nodes.AddDefaulted_GetRef();
+            node.Type = ENodeType::Leaf;
+            node.Bounds = bounds;
+            node.Leaf.Value = value;
+            return Nodes.Num() - 1;
+        }
+
+        TIdx AllocateParentNode(const TBox& bounds, uint16 left, uint16 right)
+        {
+            if (Nodes.IsFull())
+            {
+                return Index<TIdx>::None;
+            }
+
+            Node& node = Nodes.AddDefaulted_GetRef();
+            node.Type = ENodeType::Parent;
+            node.Bounds = bounds;
+            node.Parent.Left = left;
+            node.Parent.Right = right;
+            return Nodes.Num() - 1;
+        }
+
+        TFixedArray<Node, N> Nodes;
+    };
+
+    using ASDF = TFixedBVH<int32, 1024>;
+    constexpr auto s = sizeof(ASDF::Node);
+}
